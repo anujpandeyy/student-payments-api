04443b07e2b45f6da2bacbabafc2234361bf89ff
Anuj Pandey <anujpandey1701@yahoo.com>
2025-04-21 23:22:17 +0530

    model

---
+++ models/studentModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const studentSchema = mongoose.Schema({
+   name:{
+    type: String,
+    required: [true,"Name is required"],
+   },
+   stream:{
+    type: String,
+    required:[true,"Stream is required"],
+   },
+   year:{
+    type: Date,
+    required:[true,"Year is required"],
+   },
+   fees:{
+    type: Number,
+    required:[true,"Fees is required"],
+   },
+},{
+    timestamps:true,
+});
+
+module.exports = mongoose.model("Student",studentSchema);
